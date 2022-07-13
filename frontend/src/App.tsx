0589f218013c3e393476e65d3ae16844f29ad774
Raner Moreira <ranermoreira@gmail.com>
2022-07-13 16:36:34 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import Notificationbutton from "./components/notification button"
 
 function App() {
   return (
-
-<h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo!!nnn</h1>
+      <Notificationbutton />
+      <Notificationbutton />
+    </>
   )
-    
-  }
+
+}
 
 
 export default App
